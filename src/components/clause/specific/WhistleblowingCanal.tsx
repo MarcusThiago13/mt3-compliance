9e1a7968b4c30fedee5ac2ee10fd62cfec28c6d1
Skip <skip@usecurling.com>
2026-03-21 23:04:17 +0000

    Fix Input is not defined error in report form

--- src/components/clause/specific/WhistleblowingCanal.tsx
+++ src/components/clause/specific/WhistleblowingCanal.tsx
@@ -28,6 +28,7 @@ import {
 } from '@/components/ui/select'
 import { Textarea } from '@/components/ui/textarea'
 import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
 import {
   ShieldAlert,
   ListFilter,
